5eb213ff1b1017d2e98d5c66946d782771c5e18d
allen <Allen0203.Chen@senao.com>
2022-10-07 17:19:19 +0800

    github python test version3

--- test.py
+++ test.py
@@ -1,9 +1,23 @@
 
 import test_fun
 
+A=[['1231321232'],[4,5,6]]
+
 if __name__ == '__main__':
     sum = test_fun.add(5,6)
     print(sum)
-    
+    '''
     c =test_fun.q[0]
     print(c)
+
+    
+    print(A[0])
+    print(A[1][2])
+    
+    for i in A:
+        print(i)
+    '''
+    
+    for i in range(5,10):
+        print(i)
+
