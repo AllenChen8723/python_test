ab4839acbc94cd663de1d98a4798b32b0dc23de2
allen <Allen0203.Chen@senao.com>
2022-10-07 16:18:12 +0800

    github python test version2

--- test.py
+++ test.py
@@ -4,3 +4,6 @@ import test_fun
 if __name__ == '__main__':
     sum = test_fun.add(5,6)
     print(sum)
+    
+    c =test_fun.q[0]
+    print(c)
--- test_fun.py
+++ test_fun.py
@@ -1,4 +1,6 @@
 
+q = [3,6,9]
+
 def add(x,y):
     z=x+y
     return z
